6cfe6e9306e97413a90e28ba23a8174d1444b5ea
sauceaaron <aaron.evans@saucelabs.com>
2018-07-10 15:14:08 -0600

    add check sauce storage

---
+++ upload-to-sauce-storage.bat
@@ -0,0 +1,1 @@
+curl -u %SAUCE_USERNAME%:%SAUCE_ACCESS_KEY% -X POST -H 'Content-Type: application/octet-stream' https://saucelabs.com/rest/v1/storage/%SAUCE_USERNAME%/set-akamai-in-hosts-windows.bat?overwrite=true --data-binary '@src/main/resources/set-akamai-in-hosts-windows.bat'
